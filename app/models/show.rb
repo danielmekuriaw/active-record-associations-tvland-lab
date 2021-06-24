4786aa51c17e9c63c21d1ac242049e89babc6e19
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-24 01:34:56 -0500

    tvland lab solution

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        Character.all.select{|character|
+            character.show == self
+        }.map{
+            |character| character.actor.full_name
+        }.uniq
+    end
   
 end
---
+++ db/migrate/007_add_column_to_shows_genre.rb
@@ -0,0 +1,5 @@
+class AddColumnToShowsGenre < ActiveRecord::Migration[5.1]
+    def change
+        add_column :shows, :genre, :string
+    end
+end
